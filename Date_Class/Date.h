874128cae66562e5f3334de03e0552ae9098836c
garrisonglass <garrison.glass@gmail.com>
2026-03-12 21:25:12 -0400

    Added Constructor, Mutator, Accessories, Leap Year and Last day of the month in .h

--- Date_Class/Date.h
+++ Date_Class/Date.h
@@ -5,5 +5,23 @@ class Date
 		int day;
 		int month;
 		int year;
+public:
+	//Constructor
+	Date(int m = 1, int d = 1, int y = 1900);
+	//Mutator
+	void setDate(int m, int d, int y);
+
+	//Accessor
+	int getmonth() const { return month;}
+	int getday()   const { return day;  }
+	int getyear()  const { return year; }
+
+	//Leap year
+	bool isLeapYear() const;
+	bool isLeapYear(int year) const;
+
+	//Last day of month
+	int lastDay() const;
+	int lastDay(int month, int year) const;
 };
 
