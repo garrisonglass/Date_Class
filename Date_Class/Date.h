242dc9b7fe9f3b5e9049e5229715ee56fcda880e
garrisonglass <garrison.glass@gmail.com>
2026-03-15 12:14:55 -0400

    Added output functions. Lines functions up in .h

--- Date_Class/Date.h
+++ Date_Class/Date.h
@@ -1,27 +1,37 @@
-#pragma once
+#ifndef DATE_H
+#define DATE_H
+
+#include <string>
+using namespace std;
+
 class Date
 {
 	private:
 		int day;
 		int month;
 		int year;
-public:
-	//Constructor
-	Date(int m = 1, int d = 1, int y = 1900);
-	//Mutator
-	void setDate(int m, int d, int y);
+	public:
+		//Constructor
+		Date(int m = 1, int d = 1, int y = 1900);
+		//Mutator
+		void setDate(int m, int d, int y);
 
-	//Accessor
-	int getmonth() const { return month;}
-	int getday()   const { return day;  }
-	int getyear()  const { return year; }
+		//Accessor
+		int getmonth() const { return month;}
+		int getday()   const { return day;  }
+		int getyear()  const { return year; }
 
-	//Leap year
-	bool isLeapYear() const;
-	bool isLeapYear(int year) const;
+		//Leap year
+		bool isLeapYear() const;
+		bool isLeapYear(int year) const;
 
-	//Last day of month
-	int lastDay() const;
-	int lastDay(int month, int year) const;
+		//Last day of month
+		int lastDay() const;
+		int lastDay(int month, int year) const;
+
+		string toNumeric() const;
+		string toLong()    const;
+		string toEuro()    const;
 };
+#endif
 
