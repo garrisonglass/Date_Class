a9844e31d17096b01e695e5cab473d7ba7544601
garrisonglass <garrison.glass@gmail.com>
2026-03-16 21:50:29 -0400

    Added some missing brackets

--- Date_Class/Date.cpp
+++ Date_Class/Date.cpp
@@ -13,7 +13,6 @@ Date::Date(int m, int d, int y)
 		month = m;
 		day = d;
 		year = y;
-
 	}
 	else
 	{
@@ -46,10 +45,14 @@ bool Date::isLeapYear() const
 
 bool Date::isLeapYear(int year) const
 {
-	if ((year % 4 == 0 && year % 100 !=0) || (year % 400 == 0))
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	{
 		return true;
+	}
 	else
+	{
 		return false;
+	}
 }
 
 int Date::lastDay() const
@@ -60,27 +63,41 @@ int Date::lastDay() const
 int Date::lastDay(int m, int y) const
 {
 	if (m == 4 || m == 6 || m == 9 || m == 11)
+	{
 		return 30;
+	}
 	else if (m==2)
 	{
 		if (isLeapYear(y))
+		{
 			return 29;
+		}
 		else
+		{
 			return 28;
+		}
 	}
 	else
+	{
 		return 31;
+	}
 }
 
 bool Date::isValidDate(int m, int d, int y) const
 {
 	if (y < 1900 || y>2100)
+	{
 		return  false;
+	}
 	if (m < 1 || m > 12)
+	{
 		return false;
+	}
 	int maxDay = lastDay(m, y);
 	if (d < 1 || d > maxDay)
-	    return false;
+	{
+		return false;
+	}
 }
 
 string Date::toNumeric() const
